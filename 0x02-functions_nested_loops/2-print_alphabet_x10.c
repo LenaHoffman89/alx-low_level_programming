6fd864208d4d6046cfd75c47bb9789f0abfd066d
LenaHoffman89 <helenasam89@gmail.com>
2023-07-19 04:48:20 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,13 +5,15 @@
  */
 void print_alphabet_x10(void)
 {
-	char c;
-	int i = 0;
+	int i;
+	int j;
 
-	while (i <= 9)
-		for (c = 'a'; c <= 'z'; c++)
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
 		{
-		_putchar('\n');
-		i++;
+			_putchar(j);
 		}
+		_putchar('\n');
+	}
 }
