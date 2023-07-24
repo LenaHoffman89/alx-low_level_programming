8296721ee250373bc375a86548dd6f29e63676f2
LenaHoffman89 <helenasam89@gmail.com>
2023-07-24 15:20:31 -0700

    C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,39 +1,39 @@
 #include "main.h"
 /**
+ * is_numerical - checks if digit
+ * @n: number to be checked
+ * Return: 1 or 0
+ */
+int is_numerical(unsigned int n)
+{
+	return (n >= '0' && n <= '9');
+}
+
+/**
  * _atoi - function that convert a string to an integer.
  * @s: String to be converted
  * Return: integer converted from the string
  */
 int _atoi(char *s)
 {
-	int i, d, n, len, f, digit;
+	unsigned int number, i;
+	int sign;
 
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
+	sign = 1;
+	number = 0;
 
-	while (s[len] != '\0')
-		len++;
-	while (i < len && f == 0)
+	for (i = 0; *(s + i) != '\0'; i++)
 	{
-		if (s[i] == '-')
-			++d;
-		if (s[i] >= '0' && s[i] <= '9')
+		if (is_numerical(*(s + i)))
 		{
-			digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
+			number = (*(s + i) - 48) + number * 10;
+			if (s[i + 1] == ' ')
 				break;
-			f = 0;
 		}
-		i++;
+		else if (s[i] == '-')
+		{
+			sign *= -1;
+		}
 	}
-	if (f == 0)
-		return (0);
-	else
-		return (n);
+	return (number * sign);
 }
